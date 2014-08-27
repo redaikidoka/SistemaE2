8ea24915a59e558aeefbc834880591b1014c3b8f
Pol Stafford <redaikidoka@gmail.com>
2014-08-27 16:58:25 -0700

    fix new module

--- app/scripts/controllers/quickview.js
+++ app/scripts/controllers/quickview.js
@@ -7,7 +7,7 @@
  * # QuickviewCtrl
  * Controller of the sistemaE2App
  */
-angular.module('sistemaE2App')
+angular.module('sistemaApp')
   .controller('QuickviewCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
